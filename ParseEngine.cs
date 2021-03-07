ca72e313e2e170fbd2c28a3857ee9a83733e9541
Max <Max@DESKTOP-D3IVQ5F>
2021-03-07 10:07:39 +0300

    Почистил от не нужного мусора

--- ParseEngine.cs
+++ ParseEngine.cs
@@ -7,7 +7,6 @@ public class TorrentParser : Object
 {
 	public string itemUrl { get; private set; }
 	public string itemstring { get; private set; }
-	XmlNamespaceManager oManager = new XmlNamespaceManager(new NameTable());
 
 	public string FromNameToUrl(string baseUrl, string Search)
 	{
@@ -17,38 +16,15 @@ public class TorrentParser : Object
 	public HtmlDocument GetHtmlDocFromUrl(string Url)
 	{
 		HtmlWeb web = new HtmlWeb();
-		oManager.AddNamespace("ls", Url);
 		var HtmlDoc = web.Load(Url);
 		return HtmlDoc;
 	}
-	public void AddXmlNamespace(string url)
-	{
-		oManager.AddNamespace("ns", url);
-		return;
-	}
+
 	public HtmlNodeCollection GetResultDivCollection(HtmlDocument doc)
     {
 		HtmlNodeCollection Collection = doc.DocumentNode.SelectNodes("//div[@class=\"webResult item\"]");
 		return Collection;
     }
-	public ArrayList getHrefArray(HtmlNodeCollection collection)
-    {
-		ArrayList list = new ArrayList();
-		foreach (HtmlNode node in collection)
-        {
-			list.Add(node.SelectSingleNode("//p/a").Attributes["href"].Value);
 
-        }
-		return list;
-
-	}
-	public string GetItemFromArrayList(ArrayList list )
-    {
-		foreach ( var item in list)
-        {
-			itemstring = item.ToString();
-        }
-		return itemstring;
-    }
 		 
 }
--- Program.cs
+++ Program.cs
@@ -17,15 +17,27 @@ namespace TorrentParser_CLI
             Console.WriteLine("Напишите поисковый запрос в TorLook!!!");
 
             string Search = Console.ReadLine();
+
+
             Console.WriteLine("Введмте Кол-во результатов");
+
+
             string SearchCount = Console.ReadLine();
+
+
             Console.WriteLine("Введмте Задержку (в МС)");
+
+
             string Delay = Console.ReadLine();
+
             TorrentParser torrentParser = new TorrentParser();
+
             string LinkedUrl = torrentParser.FromNameToUrl("https://torlook.info/", Search);
-            // string pseudourl = "https://torlook.info/Minecraft";
-            torrentParser.AddXmlNamespace(LinkedUrl);
+
+            
+
             var doc = torrentParser.GetHtmlDocFromUrl(LinkedUrl);
+
             var collection = torrentParser.GetResultDivCollection(doc);
 
             int SearchInt = int.Parse(SearchCount);
@@ -37,8 +49,8 @@ namespace TorrentParser_CLI
                 var name = doc.DocumentNode.SelectSingleNode("//*[@id=\"resultsDiv\"]/*[contains(@class, \"webResult item\")][" + i + "]/p/a").InnerText;
                 var UrlToTorrent = doc.DocumentNode.SelectSingleNode("//*[@id=\"resultsDiv\"]/*[contains(@class, \"webResult item\")][" + i + "]/p/a").Attributes["href"].Value;
                 var size = doc.DocumentNode.SelectSingleNode("//*[@id=\"resultsDiv\"]/*[contains(@class, \"webResult item\")][" + i + "]/*[@class=\"h2\"]/*[@class=\"webResultTitle\"]/*[@class=\"size\"]").InnerText;
-                var seeders = doc.DocumentNode.SelectSingleNode("//*[@id=\"resultsDiv\"]/*[contains(@class, \"webResult item\")][2]/*[@class=\"h2\"]/*[@class=\"webResultTitle\"]/*[@class=\"torstat\"]/*[@class=\"seeders\"]").InnerText;
-                var leechers = doc.DocumentNode.SelectSingleNode("//*[@id=\"resultsDiv\"]/*[contains(@class, \"webResult item\")][2]/*[@class=\"h2\"]/*[@class=\"webResultTitle\"]/*[@class=\"torstat\"]/*[@class=\"leechers\"]").InnerText;
+                var seeders = doc.DocumentNode.SelectSingleNode("//*[@id=\"resultsDiv\"]/*[contains(@class, \"webResult item\")][" + i + "]/*[@class=\"h2\"]/*[@class=\"webResultTitle\"]/*[@class=\"torstat\"]/*[@class=\"seeders\"]").InnerText;
+                var leechers = doc.DocumentNode.SelectSingleNode("//*[@id=\"resultsDiv\"]/*[contains(@class, \"webResult item\")][" + i + "]/*[@class=\"h2\"]/*[@class=\"webResultTitle\"]/*[@class=\"torstat\"]/*[@class=\"leechers\"]").InnerText;
                 Console.WriteLine("Name: " + name);
                 Console.WriteLine("Url : " + UrlToTorrent);
                 Console.WriteLine("Size: " + size);
